8e9ecfb2ad17bebd463233bbb7313aeca1bf6766
Manideep Yama <manideep.yama@ericsson.com>
2022-07-21 14:08:25 +0200

    firstcommit

---
+++ Downloads/J2SE_MASTER_630/TestIQ.java
@@ -0,0 +1,6 @@
+class TestIQ{
+	public static void main(String arg[]){
+		float f=3.14;  //local variable
+		System.out.println("f val is :"+f);
+	}
+}
---
+++ Downloads/J2SE_MASTER_630/Testing.java
@@ -0,0 +1,17 @@
+import static java.lang.Math.*;
+import static java.lang.System.*;
+import  java.util.*;
+
+class Testing{
+	public static void main(String args[]){
+		out.println("PI Value : "+PI);
+		out.println("E value : "+E);
+		out.println	("Sqrt of 25 : "+sqrt(25));
+		out.println	("Min : "+min(10,20));
+		out.println	("Max : "+max(2.2f,3.3f));
+	}
+}
+
+
+
+
